164d9b94e4ab3261e4615525b6230b90d3c105cd
21142 <s21142@pjwstk.edu.pl>
2024-10-22 17:23:40 +0200

    Configured payload cms admin dashboard
    - prepared for self hosting next.js app
    - configured mongodb connection for payload cms

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { nextApp, nextHandler } from "./next-utils";
+import { getPayloadClient } from "./payload";
+
+const app = express();
+export const PORT = Number(process.env.PORT) || 3000;
+
+const start = async () => {
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin dashboard is running at ${cms.getAdminURL()}`);
+      },
+    },
+  });
+
+  app.use((req, res) => nextHandler(req, res));
+
+  nextApp.prepare().then(() => {
+    payload.logger.info(`Next.js app is running`);
+
+    app.listen(PORT, async () => {
+      payload.logger.info(
+        `Next.js app URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`,
+      );
+    });
+  });
+};
+
+start();
